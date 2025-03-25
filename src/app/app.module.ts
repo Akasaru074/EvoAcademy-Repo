45ec35db565d5473f7c11b35230900c5318aa57d
Ivan Morozov <vanyam.vvo@gmail.com>
2025-03-25 22:14:29 +1000

    some experiments with pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GnomeComponent } from './gnome/gnome.component';
 import { GnomeListComponent } from './gnome-list/gnome-list.component';
+import { RoommatePipePipe } from './roommate-pipe.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     GnomeComponent,
-    GnomeListComponent
+    GnomeListComponent,
+    RoommatePipePipe
   ],
   imports: [
     BrowserModule,
---
+++ src/app/roommate-pipe.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { RoommatePipePipe } from './roommate-pipe.pipe';
+
+describe('RoommatePipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new RoommatePipePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
